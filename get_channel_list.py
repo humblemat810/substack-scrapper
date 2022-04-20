dd0ad68064ddc222e843ea0c8a8dc3e0d7303336
humblemat810 <67593116+humblemat810@users.noreply.github.com>
2022-04-20 13:52:43 +0800

    Doc: README.md and images added

--- get_channel_list.py
+++ get_channel_list.py
@@ -5,34 +5,37 @@ Created on Thu Apr 14 15:51:33 2022
 @author: humblemat810
 """
 
-
+category_chosen = 'business'
+category_mapping = {"finance" : 153, 'business' : 62}
+category_number = category_mapping[category_chosen]
 """sample query
-https://substack.com/api/v1/category/public/62/all?page=5
+business : https://substack.com/api/v1/category/public/62/all?page=5
+finance : https://substack.com/api/v1/category/public/153/all?page=3
 """
 import requests
 from sqlitedict import SqliteDict
-from scrape_substack import get_meta
+from scrape_single_channel import get_meta
 import pathlib
 #%%
 
-# cnt_page = 0
-# total = 0
-# has_more = True
+cnt_page = 0
+total = 0
+has_more = True
 
-# with SqliteDict("business.sqlite") as mydict:
-#     while has_more:
+with SqliteDict(category_chosen + ".sqlite") as mydict:
+    while has_more:
         
-#         uri = "https://substack.com/api/v1/category/public/62/all?page=" + str(cnt_page)
-#         print(uri)
-#         res = requests.get(uri)
+        uri = "https://substack.com/api/v1/category/public/"+ str(category_number) +"/all?page=" + str(cnt_page)
+        print(uri)
+        res = requests.get(uri)
         
-#         total += len(res.json()['publications'])
-#         has_more = res.json()['more']
-#         for i in res.json()['publications']:
-#             mydict[i['id']] = i
-#         mydict.commit()
+        total += len(res.json()['publications'])
+        has_more = res.json()['more']
+        for i in res.json()['publications']:
+            mydict[i['id']] = i
+        mydict.commit()
         
-#         cnt_page += 1
+        cnt_page += 1
         
 #%%
 with SqliteDict("business_meta.sqlite") as metadict:
@@ -53,3 +56,15 @@ with SqliteDict("business_meta.sqlite") as metadict:
                 metadict.commit()
                 import time
                 time.sleep(5)
+import pandas as pd
+df_out = pd.DataFrame(['first_post_date', 'latest_post_date', 'n_article', 'paid'])
+records = []
+with SqliteDict(category_chosen + "_meta.sqlite") as metadict:
+    with SqliteDict(category_chosen + ".sqlite") as mydict:
+        for k, v in metadict.items():
+            d2 = {'id': k}
+            d2.update(v)
+            records.append(d2)
+            
+df_out = pd.DataFrame.from_records(records).set_index('id')
+df_out.to_csv(category_chosen + '.csv')
